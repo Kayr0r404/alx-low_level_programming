59c87a226e4d609cfc8e747078a3c79c501f5ff9
Kayr0r404 <mahlarekarabo.702@gmail.com>
2023-02-22 03:05:19 +0200

    Increment or Decrement

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_to_98 - Entry point
+ * @n: 'integer'
+ * Description: 'Write a program that prints _putchar, followed by a new line'
+ * Return: Always 0 (Success)
+ */
+void print_to_98(int n)
+{
+	int counter;
+
+	if (n < 98)
+	{
+		for (counter = n; counter <= 98; counter++)
+		{
+			printf("%d, ", counter);
+		}
+	}
+	else
+	{
+		for (counter = n; counter <= 98; counter--)
+		{
+			printf("%d, ", counter);
+		}
+	}
+}

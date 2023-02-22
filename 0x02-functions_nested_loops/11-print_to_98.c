3e1db4c25580a7272de60402079be8496b4da045
Kayr0r404 <mahlarekarabo.702@gmail.com>
2023-02-22 03:24:35 +0200

    Increment or Decrement

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,18 +10,19 @@ void print_to_98(int n)
 {
 	int counter;
 
-	if (n <= 98)
+	if (n < 98)
 	{
-		for (counter = n; counter <= 98; counter++)
+		for (counter = n; counter < 98; counter++)
 		{
 			printf("%d, ", counter);
 		}
 	}
 	else
 	{
-		for (counter = n; counter >= 98; counter--)
+		for (counter = n; counter > 98; counter--)
 		{
 			printf("%d, ", counter);
 		}
 	}
+	print("%d", counter);
 }

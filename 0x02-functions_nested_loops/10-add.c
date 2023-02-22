e675034496db7f7deaf3f974f9545cb979bdda2c
Kayr0r404 <mahlarekarabo.702@gmail.com>
2023-02-22 02:45:01 +0200

    addition

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add - Entry point
+ * @num1: 'integer'
+ * @num2: 'integer'
+ * Description: 'Write a program that prints _putchar, followed by a new line'
+ * Return: Always 0 (Success)
+ */
+int add(int num1, int num2)
+{
+	return (num1 + num2);
+}

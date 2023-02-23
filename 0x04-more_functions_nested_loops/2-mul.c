4edd39d7a7c6d055c3f7dbb625ac84d15caed71a
Kayr0r404 <mahlarekarabo.702@gmail.com>
2023-02-23 20:40:21 +0200

    produuct of two integers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ *mul - entry function
+ *@a: 'integer'
+ *@b: 'integer'
+ * Description: 'Write a function that multiplies two integers.
+ * Prototype: int mul(int a, int b)'
+ * Retrun: Product of @a and @b
+ */
+inr mul(int a, int b)
+{
+	return (a * b);
+}

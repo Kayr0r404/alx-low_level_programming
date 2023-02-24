876f6f4db95f4a49ac53fe40c5dead2a37a31324
Kayr0r404 <mahlarekarabo.702@gmail.com>
2023-02-25 00:41:19 +0200

    print numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <unistd.h>
 /**
  *print_numbers - entry point
  * Description: 'Write a function that prints the numbers,
@@ -15,16 +14,4 @@ void print_numbers(void)
 		_putchar(i);
 	}
 	_putchar('\n');
-}
-
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
 }

8db07c00c86c30ab98b8ac79677558795a03f853
Kayr0r404 <mahlarekarabo.702@gmail.com>
2023-02-24 09:10:31 +0200

    most numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -12,9 +12,7 @@ void print_most_numbers(void)
 	char num;
 
 	for (num = 48; num < 58; num++)
-		if (num == 50 && num == 52)
-			continue;
-		else
+		if (num != 50 && num != 52)
 			_putchar(num);
 	_putchar('\n');
 }

d6104b5df5345e71848ab64e0c106bb4b24b458c
Kayr0r404 <mahlarekarabo.702@gmail.com>
2023-02-24 14:40:28 +0200

    lines

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_line - entry function
+ * @n: 'argumnent only accepts an integer'
+ * Description: 'Write a function that draws a straight line in the terminal.
+ * Prototype: void print_line(int n)
+ * You can only use _putchar function to print
+ * Where n is the number of times the character _ should be printed
+ * The line should end with a \n
+ * If n is 0 or less, the function should only print \n'
+ */
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i <= n; i++)
+		if (i != 0 && i > 0)
+			_putchar('_');
+		else
+			break;
+	_putchar('\n');
+}

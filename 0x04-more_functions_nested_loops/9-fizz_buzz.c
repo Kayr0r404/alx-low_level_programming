c1a211364ee0df8151ae8dbaf4ae0b1b67b9e423
Kayr0r404 <mahlarekarabo.702@gmail.com>
2023-02-24 23:42:39 +0200

    FizzBuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -26,12 +26,12 @@ int main(void)
 		}
 		else
 		{
-			if (i % 3 == 0)
-				printf("Fizz ");
+			if (i % 3 == 0 && i % 5 == 0)
+				printf("FizzBuzz ");
 			else if (i % 5 == 0)
 				printf("Buzz ");
-			else if (i % 3 == 0 && i % 5 == 0)
-				printf("FizzBuzz ");
+			else if (i % 5 == 0)
+				printf("Buzz ");
 			else
 				printf("%d ", i);
 		}

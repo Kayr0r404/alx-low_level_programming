92abf0cbe9f0daa86ec63ff6476a0a2534e2def4
Kayr0r404 <mahlarekarabo.702@gmail.com>
2023-02-28 03:03:15 +0200

    every other char

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -10,9 +10,12 @@
 
 void puts2(char *str)
 {
-	int i;
+	int i, j = 0;
 
-	for (i = 0; i < _strlen(str); i++)
+	while (str[j] != '\0') {
+		j++;
+	}
+	for (i = 0; i < j; i++)
 		if (i % 2 == 0)
 			_putchar(str[i]);
 	_putchar('\n');

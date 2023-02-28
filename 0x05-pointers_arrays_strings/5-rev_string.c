8380946cb7e66a0bea9a60076bdc2478b08c501b
Kayr0r404 <mahlarekarabo.702@gmail.com>
2023-02-28 23:02:11 +0200

    Reverse

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+*rev_string - Entry point baba
+*@s: 'string passed'
+*Description: 'Write a function that reverses a string.
+*Prototype: void rev_string(char *s)'
+*/
+void rev_string(char *s)
+{
+	size_t length = strlen(s);
+
+	for (size_t i = 0; i < length / 2; i++)
+	{
+		char temp = s[i];
+
+		s[i] = s[length - i - 1];
+		s[length - i - 1] = temp;
+	}
+}

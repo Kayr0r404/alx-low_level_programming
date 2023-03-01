ef6ecb37c44bb06ec374682753e3cfc7c679e34a
Kayr0r404 <mahlarekarabo.702@gmail.com>
2023-03-01 20:40:50 +0200

    concatinate strings

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,44 @@
+#include "main.h"
+int len(char *str);
+
+/**
+*_strncat - Entry point
+*@dest: String
+*@src: String
+*@n: integer
+*Description: 'Write a function that concatenates two strings.
+*Prototype: char *_strncat(char *dest, char *src, int n);
+*it will use at most n bytes from src; and
+*src does not need to be null-terminated if
+*it contains n or more bytes'
+*Return: a pointer to the resulting string dest
+*/
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int lenDest = len(dest);
+	int lenSrc = len(src);
+	int i;
+
+	for (i = 0; i <= n; i++)
+		if (lenSrc >= n)
+			dest[lenDest + i] = src[i];
+	dest[lenDest + i] = '\0';
+
+	return (dest);
+}
+/**
+*len - function
+*@str: String
+*Description: 'the length of the string passed'
+*Return: String length
+*/
+
+int len(char *str)
+{
+	int lenStr = 0;
+
+	while (str[lenStr] != '\0')
+		lenStr++;
+	return (lenStr);
+}

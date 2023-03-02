a7faa861fe3a7f92e56736e44732fec14413fab7
Kayr0r404 <mahlarekarabo.702@gmail.com>
2023-03-02 23:12:31 +0200

    cap Strings

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdbool.h>
 
 /**
 *cap_string - entry
@@ -13,10 +14,13 @@
 char *cap_string(char *str)
 {
 	int i;
+	bool cond1 = str[i] >= 32 && str[i] <= 47;
+	bool cond2 = str[i] >= 9 && str[i] <= 10;
+	bool cond3 = str[i] >= 123 && str[i] <= 125;
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
-		if ((str[i] >= 32 && str[i] <= 47) || (str[i] >= 9 && str[i] <=10) || (str[i] >= 123 && str[i] <= 125))
+		if ((cond1) || (cond2) || (cond3))
 		{
 			if (str[i + 1] <= 122 && str[i + 1] >= 97)
 				str[i + 1] = str[i + 1] - 32;

86fe46c7f568309c5d428c18e9d50dd1bed0b49b
Kayr0r404 <mahlarekarabo.702@gmail.com>
2023-03-08 01:20:23 +0200

    printing chessboard

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,4 +1,7 @@
 void print_diagsums(int *a, int size);
+int _putchar(char c);
+void print_chessboard(char (*a)[8]);
+char *_memcpy(char *dest, char *src, unsigned int n);
 char *_memset(char *s, char b, unsigned int n);
 char *_strchr(char *s, char c);
 char *_memset(char *s, char b, unsigned int n);

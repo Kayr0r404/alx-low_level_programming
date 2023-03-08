d5dad1192e8b118e7d9ec1b2cc74b84d99ec3de9
Kayr0r404 <mahlarekarabo.702@gmail.com>
2023-03-08 03:11:19 +0200

    first occurrance of char

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,4 +1,5 @@
 void print_diagsums(int *a, int size);
+unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 int _putchar(char c);
 void print_chessboard(char (*a)[8]);

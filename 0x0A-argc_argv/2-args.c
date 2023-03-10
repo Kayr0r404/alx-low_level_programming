da8b20637b4f857354e6b2f56134971d41478e57
Kayr0r404 <mahlarekarabo.702@gmail.com>
2023-03-10 17:19:10 +0200

    printitng args

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Description: 'Write a program that prints all arguments it receives.
+ * All arguments should be printed, including the first one
+ * Only print one argument per line, ending with a new line'
+ * Return: 0 Always
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+		printf("%s\n", argv[i]);
+	return (0);
+}

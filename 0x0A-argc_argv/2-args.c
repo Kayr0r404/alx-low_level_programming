790c63d3286b75a4d06e9aa89264e6e333353935
Kayr0r404 <mahlarekarabo.702@gmail.com>
2023-03-10 17:21:07 +0200

    printitng args

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 /**
  * main - entry point
+ * @argc: number of arguments passed to the commandline
+ * @argv: array of char containg args passed to commandlie
  * Description: 'Write a program that prints all arguments it receives.
  * All arguments should be printed, including the first one
  * Only print one argument per line, ending with a new line'

18a152a29d981ebac9338256eb4c4a25f2ed75f1
Kayr0r404 <mahlarekarabo.702@gmail.com>
2023-03-11 03:03:44 +0200

    print number of args passed to the command-line

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *@argc: number of arguments
+ *@argv: array of arguments
+ *Description: ' Write a program that prints the number
+ *of arguments passed into it
+ *Your program should print a number, followed by a new line'
+ *Return: number of arguments passed on command line
+ */
+
+int main(int argc, char *argv[])
+{
+	long unsigned int i;
+	char *arr[1] = {"Karabo"};
+
+	for (i = 0; (sizeof(arr) / sizeof(arr[0])) > i; i++)
+	{
+		arr[0] = argv[i];
+	}
+	printf("%d\n", argc - 1);
+	return (argc);
+}

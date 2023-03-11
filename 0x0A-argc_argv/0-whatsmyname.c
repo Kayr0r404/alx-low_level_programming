ff35a4d4e9882d981dd74a7f53dd0fd23f1bb279
Kayr0r404 <mahlarekarabo.702@gmail.com>
2023-03-11 02:10:34 +0200

    print executable fine name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,6 +2,8 @@
 
 /**
  *main - Entry Point
+ *@argc: number of args pased to the command-line
+ *@argv: Array of Strings containing args passed to the command-line
  * Description: 'Write a program that prints its name,
  * followed by a new line.
  * If you rename the program, it will print the new name,
@@ -12,8 +14,8 @@
 
 int main(int argc, char *argv[])
 {
-	printf("Program name: %s\n", argv[0]);
-	
+	printf("%s\n", argv[0]);
+
 	return (argc);
 }
 

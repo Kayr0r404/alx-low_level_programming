a89b913695e6d5689cedbc7c3f912a2a86d00094
Kayr0r404 <mahlarekarabo.702@gmail.com>
2023-03-14 20:24:23 +0200

    allocate array size using malloc()

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,12 +15,20 @@ char *create_array(unsigned int size, char c)
 {
 	unsigned int i;
 	char *ptrArray;
+	
+	/*allocate array size using malloc()*/
 	ptrArray = malloc(size * sizeof(char));
+
+	/*verify if malloc is successful in allocating memory
+	 * if it is unsuccesful then NULL pointer is returned*/
+	if (ptrArray == NULL)
+		return (NULL);
 
 	if (size == 0)
 		return (NULL);
 
 	for (i = 0; i < size; i++)
+		/*store c at index i in the array*/
 		*(ptrArray + i) = c;
 
 	return (ptrArray);

d2de820b390d174216fabc4cef30c755adfed2d5
Kayr0r404 <mahlarekarabo.702@gmail.com>
2023-03-16 10:42:27 +0200

    concat string by reallocating memory

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -10,7 +10,8 @@
  * Description: 'Write a function that concatenates two strings.
  * Prototype: char *str_concat(char *s1, char *s2);
  * The returned pointer should point to a newly allocated space in memory which
- * contains the contents of s1, followed by the contents of s2, and null terminated'
+ * contains the contents of s1,
+ * followed by the contents of s2, and null terminated'
  * Return:' if NULL is passed, treat it as an empty string
  * The function should return NULL on failure
  */
@@ -18,12 +19,17 @@
 char *str_concat(char *s1, char *s2)
 {
 	char *ptrStrConcat;
+
 	unsigned int long i = 0;
 
 	ptrStrConcat = (char *) malloc(strlen(s1) * sizeof(char));
 
 	if (ptrStrConcat == NULL)
 		return (NULL);
+	else if (s1 == NULL)
+		s1 = "";
+	else if (s2 == NULL)
+		s2 = "";
 
 	while (*s1 != '\0')
 	{

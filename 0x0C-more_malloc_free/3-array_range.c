ed3c285a7144e42b6a7ed9d2b65c001724c4fdcc
Kayr0r404 <mahlarekarabo.702@gmail.com>
2023-03-18 03:39:35 +0200

    array of int, alloc mem according to diffrence of args passed

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -2,12 +2,12 @@
 #include <stdlib.h>
 
 /**
- *
  * array_range - a function that creates an array of integers.
- * @min:
- * @max:
+ * @min: string point
+ * @max: end point
  * Prototype: int *array_range(int min, int max);
- * The array created should contain all the values from min (included) to max (included), ordered from min to max
+ * The array created should contain all the values from min (included) to
+ * max (included), ordered from min to max
  * Return: the pointer to the newly created array
  * If min > max, return NULL
  * If malloc fails, return NULL

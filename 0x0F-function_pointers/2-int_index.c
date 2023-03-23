2b477868fa71a54e6d1253559615b87b9c12f801
Kayr0r404 <mahlarekarabo.702@gmail.com>
2023-03-23 04:33:04 +0200

    Pointer function: find index of int in arrayWQ

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,33 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+
+
+/**
+ * int_index - Write a function that searches for an integer.
+ * @array: given array
+ * @size: the size of the array
+ * @cmp: point to a fuctionto be used to compare values
+ * Prototype: int int_index(int *array, int size, int (*cmp)(int));
+ * Return:' the index of the first element for which
+ * the cmp function does not return 0
+ * If no element matches, return -1
+ * If size <= 0, return -1'
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	int (*pF)(int) = cmp;
+
+	if (size <= 0)
+		return (-1);
+	else if (cmp == NULL || array == NULL)
+		exit(98);
+	for (i = 0; i < size; i++)
+	{
+		if ((*pF)(array[i]))
+			return (i);
+	}
+	return (-1);
+}

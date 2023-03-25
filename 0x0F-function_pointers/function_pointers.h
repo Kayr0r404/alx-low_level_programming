f8b0bb44732771e64e0a0d8fe9efa1bb312cd29b
Kayr0r404 <mahlarekarabo.702@gmail.com>
2023-03-25 04:09:18 +0200

    Double inclusion avoided

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,6 +1,11 @@
+#ifndef Function_Pointers_H
+#define Function_Pointers_H
+
 #include <stddef.h>
+int (*get_op_func(char *s))(int, int);
 void print_name(char *name, void (*f)(char *));
 int _putchar(char c);
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
 
+#endif

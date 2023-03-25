149cde289501f42670a41a796d46a343aa30ef94
Kayr0r404 <mahlarekarabo.702@gmail.com>
2023-03-25 04:32:50 +0200

    variadic function: print ineteger

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,33 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
+/**
+ * print_numbers - function that prints numbers, followed by a new line.
+ * where separator is the string to be printed between numbers
+ * and n is the number of integers passed to the function
+ * You are allowed to use printf
+ * If separator is NULL, don’t print it
+ * Print a new line at the end of your function
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list args;
+	unsigned int i;
+	int x;
+
+	va_start(args, n);
+
+	for (i = 0; i < n; i++)
+	{
+		x = va_arg(args, int);
+
+		if (separator == NULL)
+			printf("%d", x);
+		else
+			printf("%d%s ", x, separator);
+	}
+	printf("%d\n", x);
+}

153520d660d80078dac73a28eb3e4d435d68bf6b
Kayr0r404 <mahlarekarabo.702@gmail.com>
2023-03-28 10:04:15 +0200

    header file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,13 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - 'new type struct dog with the following elements'
+ * @name: memeber of  type = char *
+ * @age: memeber of type = float
+ * @owner: memeber of type = char *
+ */
+
 struct dog
 {
 	char *name;

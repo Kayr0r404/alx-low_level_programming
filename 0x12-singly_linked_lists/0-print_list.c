192e487c1bba0f444bf6c3c58f5c1dbe6bac8bdf
Kayr0r404 <mahlarekarabo.702@gmail.com>
2023-03-30 15:48:22 +0200

    print linkedlist elements

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+#include <stddef.h>
+#include <stdio.h>
+
+/**
+ * print_list -  Write a function that prints
+ * all the elements of a list_t list.
+ * @h: linked list
+ * Prototype: size_t print_list(const list_t *h);
+ * Return: the number of nodes
+ * If str is NULL, print [0] (nil)
+ */
+
+size_t print_list(const list_t *h)
+{
+	unsigned int count = 0;
+	void *ptr = NULL;
+
+	while (h)
+	{
+		if (!h->str)
+			printf("%s %p", "[0]", ptr);
+		else
+			printf("[%u] %s", h->len, h->str);
+		count++;
+		h = h->next;
+	}
+	printf("\n");
+
+	return (count);
+}

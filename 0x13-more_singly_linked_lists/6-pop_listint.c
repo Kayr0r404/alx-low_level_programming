0d0fa99236977d19a940c89c55ac42cd1e9d7338
Kayr0r404 <mahlarekarabo.702@gmail.com>
2023-04-03 19:37:46 +0200

    pop a node in list

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -5,13 +5,14 @@
  * of a listint_t linked list, and returns the head node’s data (n).
  * @head: input list
  * Prototype: int pop_listint(listint_t **head);
- * Return: if the linked list is empty return 0
- * 1 if success
+ * Return: the element at the node before poping
+ * if the linked list is empty return 0
  */
 
 int pop_listint(listint_t **head)
 {
 	listint_t *tmp;
+	int currData = (*head)->n;
 
 	if (!*head)
 		return (0);
@@ -19,5 +20,5 @@ int pop_listint(listint_t **head)
 	tmp = (*head)->next;
 	*head = tmp;
 
-	return (1);
+	return (currData);
 }

ab4e100d6dd7a4a1d56558b5ef36c52f778ecd23
Kayr0r404 <mahlarekarabo.702@gmail.com>
2023-04-05 00:19:02 +0200

    pop a node in listd

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,38 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * insert_nodeint_at_index - function that inserts a
+ * new node at a given position.
+ * @head: input list
+ * @n: data value at the new node
+ * @idx: is the index of the list where the new node should
+ * be added. Index starts at 0
+ * Return: the address of the new node, or NULL if it failed
+ * if it is not possible to add the new node at index idx,
+ * do not add the new node and return NULL
+ */
+
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	unsigned int i;
+	listint_t *newNode = malloc(sizeof(listint_t));
+	listint_t *current;
+
+	if (!*head || !head)
+		return (NULL);
+
+	current = *head;
+
+	for (i = 0; i < idx - 1 && current; i++)
+	{
+		current = current->next;
+	}
+
+	newNode->n = n;
+	newNode->next = current->next;
+	current->next = newNode;
+
+	return (current);
+}

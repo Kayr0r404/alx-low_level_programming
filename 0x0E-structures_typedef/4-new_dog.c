f747caf03c4553da3e05a2beb395a0a33bca409d
Kayr0r404 <mahlarekarabo.702@gmail.com>
2023-05-01 09:32:14 +0200

    create a ned dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -2,27 +2,38 @@
 #include <string.h>
 #include "dog.h"
 
-dog_t *new_dog(char *name, float age, char *owner) {
+/**
+ * new_dog - function that creates a new dog.
+ * Prototype: dog_t *new_dog(char *name, float age, char *owner);
+ * @name: The name of the dog
+ * @age: age of the dog
+ * @owner: ownwer of the dog
+ * You have to store a copy of name and owner
+ * Return: NULL if the function fails
+*/
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
 	 /*Allocate memory for the new dog*/
 	dog_t *newDog = malloc(sizeof(dog_t));
 
-	if (newDog == NULL)
+	if (!newDog)
 	{
 		return (NULL);  /*Return NULL if memory allocation fails*/
 	}
 
 	/*Allocate memory for the name and copy the string*/
-	newDog->name = malloc(strlen(name) + 1);
-	if (newDog->name == NULL)
+	newDog->name = malloc(1 + strlen(name));
+	if (!newDog->name)
 	{
 		free(newDog);
-		return (NULL);  /*Return NULL if memory allocation fails*/ 
+		return (NULL);  /*Return NULL if memory allocation fails*/
 	}
 	strcpy(newDog->name, name);
 
 	/*Allocate memory for the owner and copy the string*/
 	newDog->owner = malloc(strlen(owner) + 1);
-	if (newDog->owner == NULL)
+	if (!newDog->owner)
 	{
 		free(newDog->name);
 		free(newDog);

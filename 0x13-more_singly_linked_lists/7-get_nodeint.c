7d53b6473ba5dbaa7c959bb726ea609bf987d18d
Kayr0r404 <mahlarekarabo.702@gmail.com>
2023-05-04 04:05:08 +0200

    get node at index

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -13,22 +13,19 @@
 
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-	listint_t *node = malloc(sizeof(listint_t));
 	unsigned int i = 0;
 
-	if (head == NULL || !node)
+	if (head == NULL)
 		return (NULL);
 
 	while (head)
 	{
 		if (i == index)
-		{
-			node->n = head->n;
-			node->next = NULL;
-		}
+			return (head);
+
 		head = head->next;
 		i++;
 	}
 
-	return (node);
+	return (NULL);
 }

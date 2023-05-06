f7ab27ac9930e3fda2dcbfea422eba3256f37344
Kayr0r404 <mahlarekarabo.702@gmail.com>
2023-05-06 18:56:05 +0200

    get value of bit at index i

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/*int getBinary(unsigned long int n);*/
+/**
+ * get_bit - function that returns the value of a bit at a given index.
+ * Prototype: int get_bit(unsigned long int n, unsigned int index);
+ * @num: input decimal
+ * @i: is the index, starting from 0 of the bit you want to get
+ * Return: the value of the bit at index index or -1 if an error occured
+ */
+
+int get_bit(unsigned long int num, unsigned int i)
+{
+	/* Check if the index is valid */
+	if (i >= sizeof(unsigned long int) * 8)
+		return (0);
+
+	/* Shift the number to the right by i bits */
+	num >>= i;
+
+	/* Get the least significant bit of the shifted number */
+	return (num & 1);
+}
+

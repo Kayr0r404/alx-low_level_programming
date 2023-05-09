7518f05e6c257a179e0782bfd9fa7534bab6dda8
Kayr0r404 <mahlarekarabo.702@gmail.com>
2023-05-09 05:16:01 +0200

    create a file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,48 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * create_file - a function that creates a file.
+ * Prototype: int create_file(const char *filename, char *text_content);
+ * @filename: is the name of the file to create and
+ * @text_content: is a NULL terminated string to write to the file
+ * Return: 1 on success, -1 on failure
+ * (file can not be created, file can not be written, write “fails”, etc…)
+ * The created file must have those permissions: rw-------.
+ * If the file already exists, do not change the permissions.
+ * if the file already exists, truncate it
+ * if filename is NULL return -1
+ * if text_content is NULL create an empty file
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	FILE *file;
+	size_t elementsWritten;
+
+	if (!filename)
+		return (-1);
+
+	file = fopen(filename, "w+");
+	if (!file)
+	{
+		fclose(file);
+		return (-1);
+	}
+	if (!text_content)
+	{
+		text_content = malloc(sizeof(char));
+		if (!text_content)
+			return (-1);
+		*text_content = '\0';
+	}
+	elementsWritten = fwrite(text_content, sizeof(char), sizeof(text_content) , file);
+	if (elementsWritten == 0)
+	{
+			fclose(file);
+			return (-1);
+	}
+	fclose(file);
+	return (1);
+}

33e75cafeda6aac3037e2447b18c18d4c2cbea36
Kayr0r404 <mahlarekarabo.702@gmail.com>
2023-05-09 22:09:37 +0200

    create a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -5,7 +5,7 @@
 
 /**
  * create_file - a function that creates a file.
- * Prototype: int create_file(const char *filename, char *text_content);
+ * Prototype: int create_file(const char *filename, char *text_content)
  * @filename: is the name of the file to create and
  * @text_content: is a NULL terminated string to write to the file
  * Return: 1 on success, -1 on failure
@@ -26,6 +26,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 
 	file = fopen(filename, "w+");
+	chmod(filename, 300);
 	if (!file)
 	{
 		fclose(file);

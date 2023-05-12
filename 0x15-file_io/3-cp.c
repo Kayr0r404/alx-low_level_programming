0d23b0493282a7e318537248a241ae182c80b31a
Kayr0r404 <mahlarekarabo.702@gmail.com>
2023-05-12 03:26:56 +0200

    copy to a filed

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,6 +1,5 @@
 #include "main.h"
 
-int copyFile(char *file1, char *file2);
 #define bufferSize 1024
 #define permission 0664
 
@@ -13,57 +12,35 @@ int copyFile(char *file1, char *file2);
 
 int main(int argc, char *argv[])
 {
-	int status;
+	int fileFrom, fileTo;
+	char buffer[bufferSize];
+	ssize_t nRead;
 
 	if (argc != 3)
 	{
 		dprintf(2, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-
-	status = copyFile(argv[1], argv[2]);
-	return (status);
-}
-
-/**
- * copyFile - program that copies the content of a file to another file.
- * @file1: name of a file copying from
- * @file2: mae of a file copying to
- * Return: 0 if success
-*/
-
-int copyFile(char *file1, char *file2)
-{
-	int fileFrom, fileTo;
-	char buffer[bufferSize];
-	ssize_t nRead;
-
 	/* read the file*/
-	fileFrom = open(file1, O_RDONLY);
+	fileFrom = open(argv[1], O_RDONLY);
 	if (fileFrom == -1)
 	{
-		dprintf(2, "Error: Can't read from file %s\n", file1);
-		close(fileFrom);
+		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-
-	fileTo = open(file2, O_WRONLY | O_CREAT | O_TRUNC, permission);
+	fileTo = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, permission);
 	if (fileTo == -1)
 	{
-		dprintf(2, "Error: Can't write to %s\n", file2);
-		close(fileTo);
+		dprintf(2, "Error: Can't write to %s\n", argv[2]);
 		exit(99);
 	}
-
 	nRead = read(fileFrom, buffer, bufferSize);
 	if (nRead == -1)
 	{
-		dprintf(2, "Error: Can't read from file %s\n", file2);
-		close(fileFrom);
+		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-
-	if (write(fileTo, buffer, nRead) != nRead)
+	if (write(fileTo, buffer, nRead) < nRead)
 	{
 		int clsFileFrom = close(fileFrom);
 		int clsFileTo = close(fileTo);
@@ -74,8 +51,5 @@ int copyFile(char *file1, char *file2)
 			dprintf(2, "Error: Can't close fd %d\n", fileTo);
 		exit(100);
 	}
-
 	return (0);
 }
-
-

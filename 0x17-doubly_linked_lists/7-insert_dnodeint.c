269eb354ae0342346a03be93f27acf986df0b244
Kayr0r404 <mahlarekarabo.702@gmail.com>
2023-08-17 18:21:59 +0200

    header, insert at index, delete ata index

---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,43 @@
+#include "lists.h"
+/**
+ * insert_dnodeint_at_index - function that
+ * inserts a new node at a given position.
+ * @h: input list
+ * @idx: is the index of the list where the new node should be added
+ * @n: input data
+ * Return: the address of the new node, or NULL if it failed
+*/
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *curr = *h, *new_node;
+	unsigned int i = 0;
+
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->n = n;
+	if (*h == NULL)
+	{
+		new_node->next = NULL;
+		new_node->prev = NULL;
+		return (new_node);
+	}
+
+	curr = *h;
+	while (curr->next != NULL)
+	{
+		if (idx == i + 1)
+		{
+			new_node->next = curr->next;
+			new_node->prev = curr;
+			curr->next = new_node;
+			return (new_node);
+		}
+
+		curr = curr->next;
+		i++;
+	}
+
+	return (NULL);
+}
---
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,38 @@
+#include "lists.h"
+/**
+ * delete_dnodeint_at_index - function that deletes the node
+ * at index index of a dlistint_t linked list.
+ * @head: input list
+ * @index: index of a node to be removed
+ * Return: 1 if it succeeded, -1 if it failed
+*/
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	dlistint_t *curr, *nxt;
+	unsigned int i =  0;
+
+	if (*head == NULL)
+		return (-1);
+	curr = *head;
+	if (index == 0)
+	{
+		nxt = curr->next, free(curr);
+		nxt->prev = NULL, *head = nxt;
+		return (1);
+	}
+
+	while (curr->next != NULL)
+	{
+		if (index == i + 1)
+		{
+			nxt = curr->next->next, free(curr->next);
+			curr->next = nxt, nxt->prev = curr;
+
+			return (1);
+		}
+		curr = curr->next;
+		i++;
+	}
+
+	return (-1);
+}

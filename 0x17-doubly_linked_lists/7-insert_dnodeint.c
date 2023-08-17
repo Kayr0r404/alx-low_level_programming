d4216c57df5f76aedea59df462bc48a09fd1d11b
Kayr0r404 <mahlarekarabo.702@gmail.com>
2023-08-17 18:28:48 +0200

    handle when index = 0

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -19,25 +19,22 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	new_node->n = n;
 	if (*h == NULL)
 	{
-		new_node->next = NULL;
-		new_node->prev = NULL;
+		new_node->next = NULL, new_node->prev = NULL;
 		return (new_node);
 	}
+	if (idx == 0)
+		return (add_dnodeint(h, n));
 
 	curr = *h;
 	while (curr->next != NULL)
 	{
 		if (idx == i + 1)
 		{
-			new_node->next = curr->next;
-			new_node->prev = curr;
+			new_node->next = curr->next, new_node->prev = curr;
 			curr->next = new_node;
 			return (new_node);
 		}
-
-		curr = curr->next;
-		i++;
+		curr = curr->next, i++;
 	}
-
 	return (NULL);
 }

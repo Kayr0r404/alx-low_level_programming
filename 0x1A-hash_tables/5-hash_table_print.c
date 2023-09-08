334b59ba2ef5dda0b81907abfd1a428e75526e9a
Kayr0r404 <mahlarekarabo.702@gmail.com>
2023-09-08 04:13:40 +0200

    print hash

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -1,27 +1,31 @@
 #include "hash_tables.h"
+#include <stdio.h>
 
 /**
- * hash_table_print - a function that prints a hash table.
- * @ht: is the hash table
- * You should print the key/value in the order
- * that they appear in the array of hash table
- * Order: array, list
- * If ht is NULL, don’t print anything
-*/
+ * hash_table_print - Print a hash table in Python dictionary format.
+ * @ht: The hash table to print.
+ */
 void hash_table_print(const hash_table_t *ht)
 {
 	unsigned long int i;
 	hash_node_t *curr;
+	int first_pair = 1; /*Flag to determine if it's the first pair to print*/
+
+	if (ht == NULL)
+		return;
 
 	printf("{");
-	/*go through the array*/
+	/* Go through the array */
 	for (i = 0; i < ht->size; i++)
 	{
 		curr = ht->array[i];
-		/*iterarte the lists, and printing every key/ value*/
+		/* Iterate the lists, and print every key/value pair */
 		while (curr)
 		{
-			printf("\'%s\': \'%s\'", curr->key, curr->value);
+			if (!first_pair)
+				printf(", ");
+			printf("'%s': '%s'", curr->key, curr->value);
+			first_pair = 0;
 			curr = curr->next;
 		}
 	}
